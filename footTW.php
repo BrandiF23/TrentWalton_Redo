d31c693d7cb402de39ddf1493694590723532037
Brandi Flink <brandi_flink@yahoo.com>
2016-10-27 21:20:41 -0500

    made small change to footer responsive code

--- footTW.php
+++ footTW.php
@@ -1,9 +1,11 @@
 <div class="footer">
 	<div class="footer__item">
-			<a href="https://twitter.com/TrentWalton"><img class="footer__itemImage" src="photos/twitter.png">FOLLOW ME ON TWIITER</a>
+			<a href="https://twitter.com/TrentWalton"><img class="footer__itemImage" src="photos/twitter.png">FOLLOW ME ON TWITTER</a>
 	</div>
 
-	<div class="footer__itemRight"		<a href="?"><img class="footer__itemImageRight" src="photos/rss.png">RSS FEED</a>
+	<div class="footer__itemRight">
+
+			<a href="?"><img class="footer__itemImageRight" src="photos/rss.png">RSS FEED</a>
 
 			<a href="http://paravelinc.com"><img class="footer__itemImageRight" src="photos/paravel.png">HIRE PARAVEL</a>
 	</div>
